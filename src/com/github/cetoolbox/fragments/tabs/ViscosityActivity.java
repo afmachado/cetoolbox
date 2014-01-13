8e4480234c387afff77dad12045affb6fdedd126
Jerome Pansanel <j.pansanel@pansanel.net>
2014-01-13 08:19:52 +0100

    Remove unused comment

--- src/com/github/cetoolbox/fragments/tabs/ViscosityActivity.java
+++ src/com/github/cetoolbox/fragments/tabs/ViscosityActivity.java
@@ -378,7 +378,6 @@ public class ViscosityActivity extends Activity implements
 
 	@Override
 	public void onNothingSelected(AdapterView<?> parent) {
-		/* concentrationSpin.setText(""); */
 	}
 
 	@Override
